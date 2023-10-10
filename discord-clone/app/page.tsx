3f5f0c24ec16892867f5c0d92ca593e13d256aba
Rajeev Lochan Dash <rajeevdash07@gmail.com>
2023-10-10 07:39:45 +0530

    playing around the button

--- discord-clone/app/page.tsx
+++ discord-clone/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+const state = true;
 
 export default function Home() {
   return (
-    <p className='text-3xl font-bold text-indigo-500'>
-      Discord Clone
-    </p>
+    <div>
+      <p className='text-3xl font-bold text-indigo-500'>
+        Discord Clone
+      </p>
+      <Button className={cn(
+        "bg-indigo-500",
+        state && "bg-red-500" 
+      )}>
+        Click Me
+      </Button>
+    </div>
   )
 }
